2d2f9f26e42b12ff063e5cc9826ea96a69f347f2
nicolas-ivanov <nickolas.ivanov@gmail.com>
2021-08-02 15:23:49 +0300

    1.12. Время разбивать камни

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import {siteMenu} from './view/site-menu.js';
+import {siteFilters} from './view/filters.js';
+import {tripInfo} from './view/trip-info.js';
+import {tripCost} from './view/trip-cost.js';
+import {sortingForm} from './view/sorting.js';
+import {tripPoint, createTripListTemplate} from './view/trip-point.js';
+import {newPointForm} from './view/add-new-point.js';
+import {editTripPointForm} from './view/edit-point.js';
+
+const render = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const siteHeaderElement = document.querySelector('.trip-main');
+const siteTripControlsElement = siteHeaderElement.querySelector('.trip-controls__navigation');
+
+render(siteTripControlsElement, siteMenu(), 'afterbegin');
+render(siteTripControlsElement, siteFilters(), 'beforeend');
+render(siteHeaderElement, tripInfo(), 'afterbegin');
+
+const siteTripInfoElement = siteHeaderElement.querySelector('.trip-main__trip-info');
+render(siteTripInfoElement, tripCost(), 'beforeend');
+
+const siteEventsElement = document.querySelector('.trip-events');
+render(siteEventsElement, sortingForm(), 'beforeend');
+render(siteEventsElement, createTripListTemplate(), 'beforeend');
+
+const siteEventsListElement = document.querySelector('.trip-events__list');
+render(siteEventsListElement, newPointForm(), 'beforeend');
+render(siteEventsListElement, tripPoint(), 'beforeend');
+render(siteEventsListElement, tripPoint(), 'beforeend');
+render(siteEventsListElement, tripPoint(), 'beforeend');
+render(siteEventsListElement, editTripPointForm(), 'beforeend');
