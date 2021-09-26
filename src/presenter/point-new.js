4145b25b7ce51aa74574732883ad890e9709b6fe
nicolas-ivanov <nickolas.ivanov@gmail.com>
2021-09-26 22:21:59 +0300

    8.2.1

--- src/presenter/point-new.js
+++ src/presenter/point-new.js
@@ -1,5 +1,4 @@
 import PointEditView from '../view/point-edit.js';
-import { nanoid } from 'nanoid';
 import { remove, render, RenderPosition } from '../utils/render.js';
 import { UserAction, UpdateType } from '../const.js';
 
@@ -43,9 +42,7 @@ export default class PointNew {
     this._changeData(
       UserAction.ADD_POINT,
       UpdateType.MINOR,
-      // Пока у нас нет сервера, который бы после сохранения
-      // выдывал честный id задачи, нам нужно позаботиться об этом самим
-      Object.assign({id: nanoid()}, point),
+      point,
     );
     this.destroy();
   }
